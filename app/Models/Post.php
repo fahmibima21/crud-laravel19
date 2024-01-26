ddf15c1225d56d1285718e271c817a47d175416a
fahmibima061 <fahmibima0612@gmail.com>
2024-01-26 21:17:45 +0700

    first commit

---
+++ app/Models/Post.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'image',
+        'title',
+        'content'
+    ];
+
+    public function index()
+{
+//get posts
+$posts = Post::latest()->paginate(5);
+}
+}
+
+
